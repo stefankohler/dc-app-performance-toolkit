74f5e8308f878cd49160df29a86bc0176971d16c
Stefan Kohler <stefan.kohler@gmail.com>
2020-12-24 15:00:58 +0100

    Enable global digest

--- app/extension/bitbucket/extension_ui.py
+++ app/extension/bitbucket/extension_ui.py
@@ -6,9 +6,6 @@ from selenium_ui.base_page import BasePage
 
 def app_specific_action(webdriver, datasets):
     page = BasePage(webdriver)
-    repo = datasets['repos']
-    repo_slug = repo[0]
-    project_key = repo[1]
 
     @print_timing("selenium_app_custom_action")
     def measure():
@@ -16,9 +13,9 @@ def app_specific_action(webdriver, datasets):
         @print_timing("enable_global_digest")
         def sub_measure():
             page.go_to_url(f"${BITBUCKET_SETTINGS.server_url}/plugins/servlet/digest/account/overview")
-            checkbox = page.wait_until_visible((By.ID, 'enable-digest-global'))
-            current_state = checkbox.is_selected()
-            if (not current_state):
+            page.wait_for_page_loaded()
+            checkbox = page.get_element((By.ID, "enable-digest-global"))
+            if (not checkbox.is_selected()):
                 checkbox.click()
         sub_measure()
 
