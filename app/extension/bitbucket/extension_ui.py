898268989365b5c18d2b2731a46c97e484069f28
Stefan Kohler <stefan.kohler@gmail.com>
2020-12-24 14:30:25 +0100

    Enable global digest

--- app/extension/bitbucket/extension_ui.py
+++ app/extension/bitbucket/extension_ui.py
@@ -13,6 +13,14 @@ def app_specific_action(webdriver, datasets):
     @print_timing("selenium_app_custom_action")
     def measure():
 
+        @print_timing("enable_global_digest")
+        def sub_measure():
+            page.go_to_url(f"${BITBUCKET_SETTINGS.server_url}/plugins/servlet/digest/account/overview")
+            checkbox = page.wait_until_visible((By.ID, 'enable-digest-global'))
+            current_state = checkbox.is_selected()
+            if (not current_state) checkbox.click()
+        sub_measure()
+
         @print_timing("view_email_preview")
         def sub_measure():
             page.go_to_url(f"{BITBUCKET_SETTINGS.server_url}/plugins/servlet/digest/support/email")
