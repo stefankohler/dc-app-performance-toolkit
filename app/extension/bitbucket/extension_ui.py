2f497f37404982711282457b77a4b71ad57bfc5f
Stefan Kohler <stefan.kohler@gmail.com>
2020-12-24 18:31:31 +0100

    Enable global digest

--- app/extension/bitbucket/extension_ui.py
+++ app/extension/bitbucket/extension_ui.py
@@ -3,26 +3,27 @@ from selenium_ui.conftest import print_timing
 from util.conf import BITBUCKET_SETTINGS
 
 from selenium_ui.base_page import BasePage
+from selenium_ui.bitbucket.pages.pages import Dashboard
 
 def app_specific_action(webdriver, datasets):
-    page = BasePage(webdriver)
+    host = BITBUCKET_SETTINGS.server_url
 
     @print_timing("selenium_app_custom_action")
     def measure():
 
         @print_timing("enable_global_digest")
         def sub_measure():
-            page.go_to_url(f"${BITBUCKET_SETTINGS.server_url}/plugins/servlet/digest/account/overview")
-            page.wait_for_page_loaded()
-            checkbox = page.get_element((By.ID, "enable-digest-global"))
-            if (not checkbox.is_selected()):
-                checkbox.click()
+            page = BasePage(webdriver)
+            page.go_to_url(f"{host}/plugins/servlet/digest/account/overview")
+            page.wait_until_present((By.ID, "enable-digest-global"))
+            page.execute_js("document.querySelector('#enable-digest-global').click()")
         sub_measure()
 
         @print_timing("view_email_preview")
         def sub_measure():
-            page.go_to_url(f"{BITBUCKET_SETTINGS.server_url}/plugins/servlet/digest/support/email")
-            page.wait_for_page_loaded()        
+            page = BasePage(webdriver)
+            page.go_to_url(f"{host}/plugins/servlet/digest/support/email")
+            page.wait_until_present(((By.CSS_SELECTOR, "body.digest")))        
         sub_measure()
 
     measure()
